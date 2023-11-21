95b994f456918cf9081b7413d985b0893116dba4
Iain McKay <iain.mckay@msm.io>
2023-11-21 11:58:23 +0100

    Fixing pagination

--- src/RefinableRulerZ.php
+++ src/RefinableRulerZ.php
@@ -55,7 +55,7 @@ class RefinableRulerZ
 
             if ($optimizerResult->canBeOptimized()) {
                 $target
-                    ->setFirstResult(0)
+                    ->setFirstResult(null)
                     ->setMaxResults(null)
                 ;
             }
